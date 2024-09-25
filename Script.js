2ceb12534e64bb676a3d056ec2f899fe5a62804f
abdullah345 <abdallahmohamed54655@gmail.com>
2024-09-25 16:25:03 +0300

    v9

---
+++ Script.js
@@ -0,0 +1,98 @@
+let title=document.getElementById('title');
+let price=document.getElementById('price');
+let taxes=document.getElementById('taxes');
+let ads=document.getElementById('ads');
+let discount=document.getElementById('discount');
+let total=document.getElementById('total');
+let count=document.getElementById('count');
+let category=document.getElementById('category');
+let submit=document.getElementById('submit');
+
+//gettotal
+
+function getTotal(){
+    if(price.value!=''){
+        let result=(+price.value+ + taxes.value+ +ads.value )- + discount.value
+        total.innerHTML=result;
+        total.style.background='#040';
+    }
+    else{
+        total.innerHTML='';
+        total.style.background='#a00d02';
+    }
+}
+
+//getcreate
+
+let datapro;
+if(localStorage.product!=null){
+    datapro=JSON.parse(localStorage.product)
+}else{
+    datapro=[];
+}
+
+
+
+submit.onclick=function(){
+   let newpro={
+    title:title.value,
+    price:price.value,
+    taxes:taxes.value,
+    ads:ads.value,
+    discount:discount.value,
+    total:total.innerHTML,
+    count:count.value,
+    category:category.value,
+   }
+   datapro.push(newpro);
+   localStorage.setItem('product',JSON.stringify(datapro))
+   console.log(datapro);
+   clear()
+   showdata()
+}
+
+
+//clear input
+
+
+function clear(){
+    title.value='',
+    price.value='',
+    taxes.value='',
+    ads.value='',
+    discount.value='',
+    total.innerHTML='',
+    count.value='',
+    category.value=''
+
+}
+
+
+//read data
+
+function showdata(){
+    let table='';
+    for(let i=0;i<datapro.length;i++)
+    {
+        table+=
+        `
+        <tr>
+                <td>${i}</td>
+                <td>${datapro[i].title}</td>
+                <td>${datapro[i].price}</td>
+                <td>${datapro[i].taxes}</td>
+                <td>${datapro[i].ads}</td>
+                  <td>${datapro[i].total}</td>
+                <td>${datapro[i].discount}</td>
+                <td>${datapro[i].category}</td>
+                <td><button id="update">update</button></td>
+                <td><button id="delete">delete</button></td>
+                </tr>
+        
+        `
+    }
+
+    document.getElementById('tbody').innerHTML=table;
+}
+
+showdata()
